d2dbba35b0c31d92f734ce30a028bdd5c6c00267
Abhijeet Kumar <abhijeetkumar_1992@yahoo.in>
2017-12-11 17:00:45 +0530

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var app = require("express")();
-var http = require("http").Server(app);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+
+app.set("views", path.resolve("__dirname", "views"));
 
 app.get("/", function(req, res) {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(path.resolve(__dirname, "views/index.html"));
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("chat-message", function(data) {
+    console.log(data);
+  });
+
+  socket.on("disconnect", function() {
+    console.log("Socket closed");
+  });
+
+  socket.on("connect", function() {
+    console.log("Connected");
+  });
 });
 
 http.listen(3000, function() {
